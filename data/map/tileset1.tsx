5e30832f159e3a05816452430a63093cd9629e58
Theron Halsey <theron.halsey@gmail.com>
2018-07-01 12:16:57 -0400

    mirror working

---
+++ data/map/tileset1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset1" tilewidth="32" tileheight="32" tilecount="140" columns="20">
+ <image source="../img/map/area01_level_tiles.png" width="640" height="241"/>
+</tileset>
