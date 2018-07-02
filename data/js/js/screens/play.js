7b8579518c9cf8aef1eec180266ecda4215bfa2d
Theron Halsey <theron.halsey@gmail.com>
2018-07-02 18:07:02 -0400

    older working version

--- data/js/js/screens/play.js
+++ data/js/js/screens/play.js
@@ -2,11 +2,8 @@ game.PlayScreen = me.ScreenObject.extend({
 	/**	
 	 *  action to perform on state change
 	 */
-	onResetEvent: function() {	
-		
-		// play the audio track
-		me.audio.playTrack("dst-inertexponent");
-		
+	onResetEvent: function() {
+ 
         // load a level
 		me.levelDirector.loadLevel("area01");
 		
--- js/resources.js
+++ js/resources.js
@@ -1,59 +1,39 @@
 game.resources = [
+	/**
+	 * Graphics.
+	 */
+	// the main player spritesheet
+	{name: "gripe_run_right",     type:"image",	src: "data/img/sprite/gripe_run_right.png"},
+	// the spinning coin spritesheet
+	{name: "spinning_coin_gold",  type:"image",	src: "data/img/sprite/spinning_coin_gold.png"},
+	// our enemty entity
+	{name: "wheelie_right",       type:"image",	src: "data/img/sprite/wheelie_right.png"},
+    // game font
+    { name: "PressStart2P",       type:"image", src: "data/fnt/PressStart2P.png" },
+    { name: "PressStart2P",       type:"binary", src: "data/fnt/PressStart2P.fnt"},
+	// title screen
+	{name: "title_screen",        type:"image",	src: "data/img/gui/title_screen.png"},
+	// the parallax background
+	{name: "area01_bkg0",         type:"image",	src: "data/img/area01_bkg0.png"},
+	{name: "area01_bkg1",         type:"image",	src: "data/img/area01_bkg1.png"},
+	// our level tileset
+	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
 
-    /* Graphics.
-     * @example
-     * { name: "example", type:"image", src: "data/img/example.png" },
-     */
-    { name: "tileset",         type:"image",   src: "data/img/tileset.png" },
-    { name: "background",      type:"image",   src: "data/img/background.png" },
-    { name: "clouds",          type:"image",   src: "data/img/clouds.png" },
+	/*
+	 * Maps.
+ 	 */
+	{name: "area01",              type: "tmx",	src: "data/map/area01.tmx"},
+	{name: "area02",              type: "tmx",	src: "data/map/area02.tmx"},
 
-    /* Maps.
-     * @example
-     * { name: "example01", type: "tmx", src: "data/map/example01.tmx" },
-     * { name: "example01", type: "tmx", src: "data/map/example01.json" },
-     */
-    { name: "map1",            type: "tmx",    src: "data/map/map1.tmx" },
-    { name: "map2",            type: "tmx",    src: "data/map/map2.json" },
+	/*
+	 * Background music.
+	 */
+	{name: "dst-inertexponent", type: "audio", src: "data/bgm/"},
 
-
-    /* Tilesets.
-     * @example
-     * { name: "example01", type: "tsx", src: "data/map/example01.tsx" },
-     * { name: "example01", type: "tsx", src: "data/map/example01.json" },
-     */
-    { name: "tileset",         type: "tsx",    src: "data/map/tileset.json" },
-
-
-    /* Background music.
-     * @example
-     * { name: "example_bgm", type: "audio", src: "data/bgm/" },
-     */
-    { name: "dst-gameforest",  type: "audio", src: "data/bgm/" },
-
-    /* Sound effects.
-     * @example
-     * { name: "example_sfx", type: "audio", src: "data/sfx/" }
-     */
-    { name: "cling",           type: "audio",  src: "data/sfx/" },
-    { name: "die",             type: "audio",  src: "data/sfx/" },
-    { name: "enemykill",       type: "audio",  src: "data/sfx/" },
-    { name: "jump",            type: "audio",  src: "data/sfx/" },
-
-
-    /* Atlases
-     * @example
-     * { name: "example_tps", type: "json", src: "data/img/example_tps.json" },
-     */
-    // texturePacker
-    { name: "texture",         type: "json",   src: "data/img/texture.json" },
-    { name: "texture",         type: "image",  src: "data/img/texture.png" },
-
-    /* Bitmap Font
-    * @example
-    * { name: "example_fnt", type: "image", src: "data/img/example_fnt.png" },
-    * { name: "example_fnt", type: "binary", src: "data/img/example_fnt.fnt" },
-    */
-    { name: "PressStart2P", type:"image", src: "data/fnt/PressStart2P.png" },
-    { name: "PressStart2P", type:"binary", src: "data/fnt/PressStart2P.fnt"}
+	/*
+	 * Sound effects.
+	 */
+	{name: "cling", type: "audio", src: "data/sfx/"},
+	{name: "stomp", type: "audio", src: "data/sfx/"},
+	{name: "jump",  type: "audio", src: "data/sfx/"}
 ];
